d5850ab3dc0bd501c1de44f468a694ccf589654e
attila <attila>
2016-09-13 21:40:29 +0200

    vhost kivetele

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
-var vhost = require('vhost');
 
 var app = express();
 
-// api subdomain --> http://api.mydomain.com/...
-var api = express.Router();
 // elv csak API-knal lehet CORS tamadas, szval nem kell az osszes URL-re betenni a middleware-t TODO: utanaolvasni CORS-nak
-api.use('/', require('cors'));
+app.use('/', require('cors'));
 
-app.use(vhost('api.*', api));
+// CSRF tamadas elleni vedekezes, TODO: utanaolvasni CSRF-nek
+// cookie-parser es session-connect utan kell lennie!!!
+// TODO ez igy az angularnak nem eleg, hanem headerek-kel kell szorakozni: https://docs.angularjs.org/api/ng/service/$http#cross-site-request-forgery-xsrf-protection
+app.use(require('csurf'));
+app.use(function (req, res, next) {
+    res.locals._csrfToken = req.csrfToken();
+    next();
+});
 
 require('./routes')(app);
